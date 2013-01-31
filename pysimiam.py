0bab8aeecfefd8eb04f19f45a2f9fd121265f6d7
Jonathan Whitten <jon@gmail.com>
2013-01-31 18:40:00 -0500

    Added BitmapButtons and sim placeholder

--- pysimiam.py
+++ pysimiam.py
@@ -1,25 +1,48 @@
 import wx
+import os
 
-class PySimianApp(wx.App):
+class PySimiamApp(wx.App):
 	def OnInit(self):
-		self.frame = PySimianFrame(None, size=wx.Size(800,600), title="PySimian")
+		self.frame = PySimiamFrame(None, size=wx.Size(800,600), title="PySimiam")
 		self.SetTopWindow(self.frame)
 		self.frame.Show()
 
 		return True
 
-#end PySimianApp class
+#end PySimiamApp class
 
-class PySimianFrame(wx.Frame):
+class PySimiamFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         kwds["style"] = wx.DEFAULT_FRAME_STYLE
         wx.Frame.__init__(self, *args, **kwds)
 
         #Objects
-        #self.simulator = Simulator(self)
 
         #Interface buttons
-        #self.playButton = wx.ImageButton(self)
+        # Reset
+        path = os.path.abspath("./res/image/arrow-left-double.png")
+        bmp = wx.Bitmap(path, wx.BITMAP_TYPE_PNG)
+        self.resetButton = wx.BitmapButton(self, id=wx.ID_ANY, \
+                bitmap=bmp, size=(32,32))
+
+        # Play
+        path = os.path.abspath("./res/image/arrow-right.png")
+        bmp = wx.Bitmap(path, wx.BITMAP_TYPE_PNG)
+        self.playButton = wx.BitmapButton(self, id=wx.ID_ANY, \
+                bitmap=bmp, size=(32, 32))
+
+        # Pause
+        path = os.path.abspath("./res/image/media-playback-pause-7.png")
+        bmp = wx.Bitmap(path, wx.BITMAP_TYPE_PNG)
+        self.pauseButton = wx.BitmapButton(self, id=wx.ID_ANY, \
+                bitmap=bmp, size=(32, 32))
+
+
+        # Simulation Panel (now just a text placeholder)
+        self.simulator = wx.Panel(self)
+        self.simulator.SetBackgroundColour('grey')
+
+        wx.StaticText(self.simulator, label="hello")
 
         # Create layouts to arrange objects
         self.__do_layout()
@@ -32,10 +55,26 @@ class PySimianFrame(wx.Frame):
 
         # Create status bar with intro messages
         self.CreateStatusBar()
-        self.PushStatusText("Welcome to PySimian") 
+        self.PushStatusText("Welcome to PySimiam") 
 
     def __do_layout(self):
-        pass 
+        # Create main vertical sizer 'mainsizer'
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+
+
+
+        # Layout buttons
+        buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
+        buttonSizer.Add(self.resetButton, 0, wx.ALL, 2)
+        buttonSizer.Add(self.playButton, 0, wx.ALL, 2)
+        buttonSizer.Add(self.pauseButton, 0, wx.ALL, 2)
+        mainSizer.Add(buttonSizer, 0, wx.EXPAND)
+
+        #Layout Simulator
+        mainSizer.Add(self.simulator, 1, wx.EXPAND|wx.ALIGN_CENTER|wx.ALL, 5)
+
+        # Set 'mainsizer' as the sizer for the frame
+        self.SetSizer(mainSizer)
 
     def __create_toolbars(self):
         #MenuBar	
@@ -44,11 +83,8 @@ class PySimianFrame(wx.Frame):
         self.filem.Append(wx.ID_OPEN, "Open Supervisor\tCtrl+O")
         self.filem.AppendSeparator()
 
-        #self.filem.Append(ID_QUIT, "Exit", "Quit the Program")
         self.filem.Append(wx.ID_CLOSE, "Exit", "Quit the Program" )
-
         self.menu_bar.Append(self.filem, "&File")
-
         self.SetMenuBar(self.menu_bar)
 
         self.Bind(wx.EVT_MENU, self.OnClose, id=wx.ID_CLOSE)
@@ -58,9 +94,7 @@ class PySimianFrame(wx.Frame):
         pass
  
 
-
     # Event Handlers
-
 
     def OnOpen(self, event):
         event.Skip()
@@ -68,10 +102,10 @@ class PySimianFrame(wx.Frame):
     def OnClose(self, event):
         self.Destroy()
 
-#end PySimianFrame class
+#end PySimiamFrame class
 
 if __name__ == "__main__":
     #provider = wx.SimpleHelpProvider()
     #wx.HelpProvider_Set(provider)
-    app = PySimianApp(False)
+    app = PySimiamApp(False)
     app.MainLoop()
