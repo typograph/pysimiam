0b7780a8bd200c5e96f18d015f2956a715c589a8
Tim Fuchs <typograph@elec.ru>
2013-02-06 15:34:09 +0100

    Added a Qt4 version of the application

---
+++ qtsimiam.py
@@ -0,0 +1,105 @@
+"""QtSimiam
+Author: Tim Fuchs
+Description: This is the top-level application for QtSimiam.
+"""
+import sys
+sys.path.insert(0, './scripts')
+from PyQt4 import QtGui, QtCore
+import os
+from qtrenderer import QtRenderer
+
+import simulator as sim
+import threading
+
+BITMAP_WIDTH = 600
+BITMAP_HEIGHT = 800
+
+class SimulationWidget(QtGui.QMainWindow):
+    def __init__(self,parent=None):
+        QtGui.QMainWindow.__init__(self,parent)
+        self.setWindowTitle("QtSimiam")
+        self.resize(BITMAP_WIDTH,BITMAP_HEIGHT)
+        
+        self.__create_toolbar()
+        self.__create_menu()
+        self.__create_statusbar()
+        # Create status bar with intro messages
+        self.statusBar().showMessage("Welcome to QtSimiam") 
+        
+        
+        scrollArea = QtGui.QScrollArea(self)
+        self.setCentralWidget(scrollArea)
+        self.viewer = SimulatorViewer()
+        scrollArea.setWidget(self.viewer)
+        scrollArea.setWidgetResizable(True)
+        self.viewer.show()
+        
+        # create the simulator thread
+        self.simulatorThread = sim.Simulator(self.viewer.renderer,self.viewer.update)
+        self.simulatorThread.start()
+
+    def __create_toolbar(self):
+        
+        tbar = QtGui.QToolBar("Control",self)
+        tbar.addAction(QtGui.QIcon("./res/image/arrow-left-double.png"),"Rewind")
+        tbar.addAction(QtGui.QIcon("./res/image/arrow-right.png"),"Run")
+        tbar.addAction(QtGui.QIcon("./res/image/media-playback-pause-7.png"),"Pause")
+        self.addToolBar(tbar)
+
+    def __create_menu(self):
+        menu = QtGui.QMenuBar(self)
+        self.setMenuBar(menu)
+        
+        file_menu = menu.addMenu("&File")
+        file_menu.addAction(QtGui.QIcon.fromTheme("document-open"),
+                            "Open Supervisor",
+                            self.onOpen,
+                            QtGui.QKeySequence(QtGui.QKeySequence.Open))
+                            
+        file_menu.addSeparator()
+        file_menu.addAction(QtGui.QIcon.fromTheme("application-exit"),
+                            "Exit",
+                            self.close,
+                            QtGui.QKeySequence(QtGui.QKeySequence.Quit)
+                            ).setToolTip("Quit the Program")
+                            
+    def __create_statusbar(self):      
+        self.setStatusBar(QtGui.QStatusBar())
+
+    def closeEvent(self,event):
+        self.simulatorThread.stop()
+        super(SimulationWidget,self).closeEvent(event)
+
+    # Slots
+    @QtCore.pyqtSlot()
+    def onOpen(self):
+        # Load new definition
+        pass
+
+#end PySimiamFrame class
+
+class SimulatorViewer(QtGui.QFrame):
+    def __init__(self, parent = None):
+        super(SimulatorViewer, self).__init__(parent)
+        self.__bitmap = QtGui.QPixmap(BITMAP_WIDTH, BITMAP_HEIGHT)
+        self.__blt_bitmap = QtGui.QPixmap(BITMAP_WIDTH,BITMAP_HEIGHT)
+        self.renderer = QtRenderer(self.__blt_bitmap)
+        self.event = threading.Event()
+        self.event.set()
+
+    def __delete__(self):
+        self.event.set()
+
+    def paintEvent(self, event):
+        super(SimulatorViewer, self).paintEvent(event)
+        painter = QtGui.QPainter(self)
+        painter.drawPixmap(0,0,self.__blt_bitmap)
+        
+    def setBitmap(self):
+        pass
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    simWidget = SimulationWidget()
+    simWidget.show()
+    app.exec_()
