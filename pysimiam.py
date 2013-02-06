8ed141d1f5bc608013d9654321e68f6a5ac53b76
Jonathan Whitten <jon@tau.(none)>
2013-02-06 06:06:23 -0500

    Refactored pysimiam.py into PEP8

--- pysimiam.py
+++ pysimiam.py
@@ -1,6 +1,6 @@
 """PySimiam
 Author: Jonathan Whitten
-ChangeDate: 2 FEB 2013; 1134EST
+ChangeDate: 5 FEB 2013; 2204EST
 Description: This is the top-level application for PySimiam.
 """
 import sys
@@ -14,7 +14,8 @@ import simulator as sim
 
 class PySimiamApp(wx.App):
     def OnInit(self):
-        self.frame = PySimiamFrame(None, size=wx.Size(800,600), title="PySimiam")
+        self.frame = PySimiamFrame(None, size=wx.Size(BITMAP_WIDTH,
+BITMAP_HEIGHT), title="PySimiam")
         self.SetTopWindow(self.frame)
         self.frame.Show()
 
@@ -27,11 +28,10 @@ ID_PLAY = wx.NewId()
 ID_PAUSE = wx.NewId()
 ID_RESET = wx.NewId()
 
-BITMAP_WIDTH = 600
-BITMAP_HEIGHT = 800
+BITMAP_WIDTH = 800
+BITMAP_HEIGHT = 600
 
 class PySimiamFrame(wx.Frame):
-    
     def __init__(self, *args, **kwds):
         kwds["style"] = wx.DEFAULT_FRAME_STYLE
         wx.Frame.__init__(self, *args, **kwds)
@@ -39,36 +39,47 @@ class PySimiamFrame(wx.Frame):
         #Objects
 
         # ImageList
-        self.bitmap = wx.EmptyBitmap(BITMAP_WIDTH, BITMAP_HEIGHT)
-        dc = wx.MemoryDC(self.bitmap)
-        self.renderer = wxGCRenderer(dc)
+        self._bitmap = wx.EmptyBitmap(BITMAP_WIDTH, BITMAP_HEIGHT)
+        dc = wx.MemoryDC(self._bitmap)
+        self._renderer = wxGCRenderer(dc)
+
+        #self._image_index = 0
+        #self._bitmaplist = []
+        #_bitmap = wx.EmptyBitmap(BITMAP_WIDTH, BITMAP_HEIGHT)
+        #self._bitmaplist.append(_bitmap)
+        #_bitmap = wx.EmptyBitmap(BITMAP_WIDTH, BITMAP_HEIGHT)
+        #self._bitmaplist.append(_bitmap)
+
+        #Initialize dcs for renderer
+        #dc = wx.MemoryDC(self._bitmap)
+        #self._renderer = wxGCRenderer(dc)
 
         #Interface buttons
         # Reset
         path = os.path.abspath("./res/image/arrow-left-double.png")
         bmp = wx.Bitmap(path, wx.BITMAP_TYPE_PNG)
-        self.resetButton = wx.BitmapButton(self, id=ID_RESET, \
+        self._reset_button = wx.BitmapButton(self, id=ID_RESET, \
                 bitmap=bmp, size=(32,32))
 
         # Play
         path = os.path.abspath("./res/image/arrow-right.png")
         bmp = wx.Bitmap(path, wx.BITMAP_TYPE_PNG)
-        self.playButton = wx.BitmapButton(self, id=ID_PLAY, \
+        self._play_button = wx.BitmapButton(self, id=ID_PLAY, \
                 bitmap=bmp, size=(32, 32))
 
         # Pause
         path = os.path.abspath("./res/image/media-playback-pause-7.png")
         bmp = wx.Bitmap(path, wx.BITMAP_TYPE_PNG)
-        self.pauseButton = wx.BitmapButton(self, id=ID_PAUSE, \
+        self._pause_button = wx.BitmapButton(self, id=ID_PAUSE, \
                 bitmap=bmp, size=(32, 32))
 
 
         # Simulation Panel (now just a text placeholder)
-        self.viewer = SimulatorViewer(self)
+        self._viewer = SimulatorViewerPanel(self)
 
         # create the simulator thread
-        self.simulatorThread = sim.Simulator(self, self.renderer, wx.ID_ANY)
-        self.simulatorThread.start()
+        self._simulator_thread = sim.Simulator(self, self._renderer, wx.ID_ANY)
+        self._simulator_thread.start()
 
 
         # Create layouts to arrange objects
@@ -76,13 +87,13 @@ class PySimiamFrame(wx.Frame):
 
         # Create any drop-down menu bars and toolbars
         self.__create_toolbars()
-    
+
         # Set any special properties of the frame
         self.__set_properties()
 
         # Create status bar with intro messages
         self.CreateStatusBar()
-        self.PushStatusText("Welcome to PySimiam") 
+        self.PushStatusText("Welcome to PySimiam")
 
     def __do_layout(self):
         # Create main vertical sizer 'mainsizer'
@@ -90,67 +101,80 @@ class PySimiamFrame(wx.Frame):
 
         # Layout buttons
         buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
-        buttonSizer.Add(self.resetButton, 0, wx.ALL, 2)
-        buttonSizer.Add(self.playButton, 0, wx.ALL, 2)
-        buttonSizer.Add(self.pauseButton, 0, wx.ALL, 2)
+        buttonSizer.Add(self._reset_button, 0, wx.ALL, 2)
+        buttonSizer.Add(self._play_button, 0, wx.ALL, 2)
+        buttonSizer.Add(self._pause_button, 0, wx.ALL, 2)
         mainSizer.Add(buttonSizer, 0, wx.EXPAND)
 
         # Layout Simulator
-        mainSizer.Add(self.viewer, 1, wx.EXPAND|wx.ALIGN_CENTER|wx.ALL, 5)
+        mainSizer.Add(self._viewer, 1, wx.EXPAND|wx.ALIGN_CENTER|wx.ALL, 5)
 
         # Set 'mainsizer' as the sizer for the frame
         self.SetSizer(mainSizer)
 
     def __create_toolbars(self):
-        #MenuBar    
-        self.menu_bar = wx.MenuBar()
-        self.filem = wx.Menu()
-        self.filem.Append(wx.ID_OPEN, "Open Supervisor\tCtrl+O")
-        self.filem.AppendSeparator()
+        #MenuBar
+        self._menu_bar = wx.MenuBar()
+        self._filem = wx.Menu()
+        self._filem.Append(wx.ID_OPEN, "Open Supervisor\tCtrl+O")
+        self._filem.AppendSeparator()
 
-        self.filem.Append(wx.ID_CLOSE, "Exit", "Quit the Program" )
-        self.menu_bar.Append(self.filem, "&File")
-        self.SetMenuBar(self.menu_bar)
+        self._filem.Append(wx.ID_CLOSE, "Exit", "Quit the Program" )
+        self._menu_bar.Append(self._filem, "&File")
+        self.SetMenuBar(self._menu_bar)
 
-        self.Bind(wx.EVT_MENU, self.onClose, id=wx.ID_CLOSE)
-        self.Bind(wx.EVT_MENU, self.onOpen, id=wx.ID_OPEN)
+        self.Bind(wx.EVT_MENU, self._on_close, id=wx.ID_CLOSE)
+        self.Bind(wx.EVT_MENU, self._on_open, id=wx.ID_OPEN)
 
     def __set_properties(self):
-        self.Bind(wx.EVT_BUTTON, self.onButton, id=wx.ID_ANY) 
-        self.Bind(wx.EVT_CLOSE, self.onClose)
-        self.Bind(sim.EVT_VIEWER_EVENT, self.updateViewer)
+        self.Bind(wx.EVT_BUTTON, self._on_button, id=wx.ID_ANY)
+        self.Bind(wx.EVT_CLOSE, self._on_close)
+        self.Bind(sim.EVT_VIEWER_EVENT, self._update_viewer)
 
     # Methods
 
     # Event Handlers
-    def updateViewer(self, event):
-        self.viewer.paintNow(self.bitmap)
+    def _update_viewer(self, event):
+        #Swap index for renderer
+        #dc = wx.MemoryDC(self._bitmaplist[self.__swap_index()])
+        #self._renderer.set_dc(dc)
 
-    def onButton(self, event):
-        eventId = event.GetId()
+        #paint bitmap to panel
+        self._viewer.paint_now(self._bitmap)
+        #self._viewer.paint_now(self._bitmaplist[self._image_index])
 
-        if eventId == ID_PLAY:
-            print 'ButtonPress: play'
-        elif eventId == ID_PAUSE:
-            print 'ButtonPress: pause'
-        elif eventId == ID_RESET:
+        #Swap viewer index
+        #self._image_index = self.__swap_index()
+
+    #def __swap_index(self):
+    #    if self._image_index == 0:
+    #        return 1
+    #    else:
+    #        return 0
+
+    def _on_button(self, event):
+        event_id = event.GetId()
+
+        if event_id == ID_PLAY:
+            self._simulator_thread.start_simulation()
+        elif event_id == ID_PAUSE:
+            self._simulator_thread.pause_simulation()
+        elif event_id == ID_RESET:
             print 'ButtonPress: reset'
         else:
             print 'ButtonPress: unknown'
 
         event.Skip()
-        #End onButton
+        #End _on_button
 
-    def onOpen(self, event):
+    def _on_open(self, event):
         event.Skip()
+        #End _on_open
 
-        #End onOpen
-
-    def onClose(self, event):
+    def _on_close(self, event):
         # Stop simulator thread
-        self.simulatorThread.Stop()
+        self._simulator_thread.Stop()
         self.Destroy()
-
         #End onClose
 
 #end PySimiamFrame class
@@ -158,22 +182,42 @@ class PySimiamFrame(wx.Frame):
 class SimulatorViewer(wx.ScrolledWindow):
     def __init__(self, parent):
         super(SimulatorViewer, self).__init__(parent)
+
+        #Create Subpanel for scrolling feature
+        self._viewer = SimulatorViewerPanel(self)
+
+        #Create layout
+        sizer = wx.BoxSizer()
+        sizer.Add(self._viewer, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+
+        #Add scrollbars
         self.__set_properties()
-        self.__bitmap = wx.EmptyBitmap(BITMAP_WIDTH, BITMAP_HEIGHT)
-        self.__bitmap_dc = wx.MemoryDC(self.__bitmap)
 
     def __set_properties(self):
-        self.SetScrollbars(1,1,1,1)        
-        self.Bind(wx.EVT_PAINT, self.onPaint)
+        self.SetScrollbars(1,1,1,1)
 
-    # Methods
-    def paintNow(self, bmp):
-        self.__bitmap_dc.DrawBitmap(bmp, 0, 0, False) # no mask
-        self.onPaint(None)
+    def paint_now(self, bmp):
+        self._viewer.paint_now(bmp)
 
-    def onPaint(self, event):
+
+class SimulatorViewerPanel(wx.Panel):
+    def __init__(self, parent):
+        size = wx.Size(BITMAP_WIDTH, BITMAP_HEIGHT)
+        super(SimulatorViewerPanel, self).__init__(parent, size=size)
+        self.__set_properties()
+
+    def __set_properties(self):
+        #self.SetBackgroundColour(wx.Colour(255,255,255))
+        self.Bind(wx.EVT_PAINT, self._on_paint)
+
+    def paint_now(self, bmp):
         dc = wx.ClientDC(self)
-        dc.DrawBitmap(self.__bitmap, 0, 0, False) # no mask
+        bdc = wx.BufferedDC(dc)
+        bdc.DrawBitmap(bmp, 0, 0, False)
+
+    def _on_paint(self, event):
+        pass #won't erase background
 
 
 if __name__ == "__main__":
