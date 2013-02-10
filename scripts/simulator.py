55702ac5c187868c49bf0e234dfae1f4c61af108
Tim Fuchs <typograph@elec.ru>
2013-02-10 22:17:58 +0100

    Fixed merge conflicts in the simulator

--- scripts/simulator.py
+++ scripts/simulator.py
@@ -37,23 +37,10 @@ class Simulator(threading.Thread):
 
         self._render_lock = threading.Lock()
 
-        # Zoom on scene - Move to read_config later
-        self._renderer.set_zoom_level(130)
-        self._renderer.set_screen_pose(pose.Pose(-1.6,-1.5,0))
-
         # World objects
         self._robots = []
         self._obstacles = []
 
-        #test code
-#        self._robots = [ khepera3.Khepera3(pose.Pose(200.0, 250.0, 0.0)), ]
-#        self._robots[0].set_wheel_speeds(18,16)
-#        self._obstacles = [
-#            simobject.Polygon(pose.Pose(200,200,0),[(-10,0),(0,-10),(10,0),(0,10)],0xFF0000),
-#            simobject.Polygon(pose.Pose(300,100,0.1),[(-10,0),(0,-10),(10,0),(0,10)],0xFF0000),
-#            simobject.Polygon(pose.Pose(100,300,0.4),[(-10,0),(0,-10),(10,0),(0,10)],0xFF0000)
-#            ]
-        #end test code
         self._world = None
 
     #def __delete__(self):
@@ -72,7 +59,8 @@ class Simulator(threading.Thread):
         except Exception, e:
             raise Exception('[Simulator.read_config] Failed to parse ' + config \
                 + ': ' + str(e))
-        self.construct_world()
+        else:
+            self.construct_world()
 
     def construct_world(self):
         if self._world is None:
@@ -98,41 +86,28 @@ class Simulator(threading.Thread):
             else:
                 raise Exception('[Simulator.__init__] Unknown object: '
                                 + str(thing_type))
+                                
         self._render_lock.release()
         self.__time = 0.0
         if self._robots == None:
             raise Exception('[Simulator.__init__] No robot specified!')
         else:
-            self._robots[0].set_wheel_speeds(1.2,1.6)
+            for robot in self._robots:
+                robot.set_wheel_speeds(1.2,1.6)
             self.focus_on_world()
             self.draw()
-
-        # Test code - add some motion to robots
-        for robot in self._robots:
-            robot.set_wheel_speeds(3,2)
 
     def run(self):
         print 'starting simulator thread'
 
-        time_constant = 0.02  # 100 milliseconds
+        time_constant = 0.02  # 20 milliseconds
         
         self._render_lock.acquire()
         self._renderer.clear_screen() #create a white screen
         self.updateView()
         self._render_lock.release()
 
-        #self.draw() # Draw at least once (Move to open afterwards)
         while not self.__stop:
-            sleep(time_constant)
-            if self.__state != RUN:
-                continue
-            for robot in self._robots:
-                robot.move_to(robot.pose_after(time_constant))
-
-            if self.check_collisions():
-                print "Collision detected!"
-                self.__state = PAUSE
-                #self.__stop = True
 
             sleep(time_constant/self.__time_multiplier)
 
@@ -140,12 +115,10 @@ class Simulator(threading.Thread):
                 for robot in self._robots:
                     robot.move_to(robot.pose_after(time_constant))
                 self.__time += time_constant
-                #if self.check_collisions():
-                    #print "Collision detected!"
+                if self.check_collisions():
+                    print "Collision detected!"
+                    self.__state = PAUSE
                     #self.__stop = True
-
-            # Draw to buffer-bitmap
-            self.draw()
 
             # Draw to buffer-bitmap
             self.draw()
@@ -168,7 +141,6 @@ class Simulator(threading.Thread):
             robot.draw(self._renderer)
             for s in robot.ir_sensors:
                 s.draw(self._renderer)
-        #end test code
 
         self.updateView()
         self._render_lock.release()
