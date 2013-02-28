af6348910cd9c4a07b0610c7aec22823dfbaa7be
Jonathan Whitten <jon@gmail.com>
2013-02-27 19:59:09 -0500

    Updated tutorial and files

--- controllers/avoidobstacles.py
+++ controllers/avoidobstacles.py
@@ -25,65 +25,36 @@ class AvoidObstacles(Controller):
         self.ki = params.gains.ki
         self.kd = params.gains.kd
 
+        # Week 4 code 
+        # These variables will be used to make calculations
         self.angles = params.sensor_angles
         self.weights = [(math.cos(a)+1.1) for a in self.angles]
-
-    #User-defined function
-    def calculate_new_goal(self, distances):
-        """Determines a new goal for the robot based on which sensors are active"""
-        
-        angle = 0.0
-        weightdist = 0.0
-        for i in range(len(distances)):
-            angle += self.angles[i]*distances[i]*self.weights[i]
-            weightdist += distances[i]*self.weights[i] 
-
-        angle = angle/weightdist #average angle to the clear
-        angle = angle + self.robottheta
-
-        #if angle < 0.0:
-        #    angle += 2*math.pi
-        #elif angle > 2*math.pi:
-        #    angle -= 2*math.pi
-
-        print angle
-        return angle
-
-    def calculate_new_velocity(self, distances):
-        """Adjusts robot velocity based on distance to object"""
-        mindist = min(distances)
-        
-        vel = max(min(mindist/0.29*0.4, 0.4), 0.1) 
-        return vel 
+        # End week 4 code
 
     def execute(self, state, dt):
         """Executes avoidance behavior based on state and dt. 
         state --> supevisor set ui_params
-
         dt --> supervisor set timestep
-
         return --> unicycle model list [velocity, omega]"""
+
+        #Begin week 4 code
+
+        #1. Store the state values for the robot
         self.robotx, self.roboty, self.robottheta = state.pose
 
-        #Non-global goal
-        theta = self.calculate_new_goal(state.sensor_distances) #user defined function
-        v_ = self.calculate_new_velocity(state.sensor_distances) #user defined function
+        #2. Make calculations for a new goal 
+        #3. Calculate proportional error
+        #4. Correct for angles (angle may be greater than PI) with atan2
+        #5. Calculate integral error
+        #6. Calculate differential error
+        # store error in self.e_1
 
-        #1. Calculate simple proportional error
-        error = theta - self.robottheta
+        #7. Calculate desired omega
+        #Make sure you modify these variables
 
-        #2. Correct for angles (angle may be greater than PI)
-        error = math.atan2(math.sin(error), math.cos(error))
-
-        #3. Calculate integral error
-        self.E += error*dt
-
-        #4. Calculate differential error
-        dE = (error - self.error_1)/dt
-        self.error_1 = error #updates the error_1 var
-
-        #5. Calculate desired omega
-        w_ = self.kp*error + self.ki*self.E + self.kd*dE
-
-        #6. Return solution
+        v_ = 0
+        w_ = 0 
+        
+        #End week 4 code
+        #8. Return solution
         return [v_, w_]
