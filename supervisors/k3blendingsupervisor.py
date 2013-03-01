40757a6b17cb3405d3929ab8bfd8bc6bfcc61f38
Tim Fuchs <typograph@elec.ru>
2013-03-01 15:00:12 +0100

    Added a blending supervisor (week 5)

---
+++ supervisors/k3blendingsupervisor.py
@@ -0,0 +1,52 @@
+from khepera3 import K3Supervisor
+from math import sqrt, cos, pi
+
+class K3BlendingSupervisor(K3Supervisor):
+    """K3Default supervisor creates two controllers: gotogoal and avoidobstacles. This module is intended to be a template for student supervisor and controller integration"""
+    def __init__(self, robot_pose, robot_info):
+        """Creates an avoid-obstacle controller and go-to-goal controller"""
+        K3Supervisor.__init__(self, robot_pose, robot_info)
+
+        #Add controllers ( go to goal is default)
+        self.ui_params.sensor_angles = [pose.theta for pose in robot_info.ir_sensors.poses]
+        self.avoidobstacles = self.add_controller('avoidobstacles.AvoidObstacles', self.ui_params)
+        self.gtg = self.add_controller('gotogoal.GoToGoal', self.ui_params.gains)
+
+        self.current = self.gtg
+
+    def execute(self, robot_info, dt):
+        """Blend the behaviour of several controllers"""
+        # Copied from Supervisor
+        self.robot = robot_info
+        self.pose_est = self.estimate_pose()
+
+        # Check if we are in place
+        distance_from_goal = sqrt((self.pose_est.x - self.ui_params.goal.x)**2 + (self.pose_est.y - self.ui_params.goal.y)**2)
+        if distance_from_goal < self.robot.wheels.base_length/2:
+            return (0,0)
+
+        # Fill parameters for the controllers
+        self.ui_params.pose = self.pose_est
+        self.ui_params.sensor_distances = self.get_ir_distances()
+
+        # now instead of choosing one controller, blend results
+        distmin = min(self.ui_params.sensor_distances)
+        distmax = self.robot.ir_sensors.rmax
+        
+        distance_ratio = distmin/distmax     
+        if distance_ratio > 1:
+            distance_ratio = 1
+        # Forget about the goal at 0.2 ratio
+        distance_ratio -= 0.2
+        distance_ratio /= 0.8
+
+        weight_avo = 0.5*(1 + cos(pi*distance_ratio))
+            
+        v_gtg, w_gtg = self.gtg.execute(self.ui_params,dt) #execute go-to-goal
+        v_avo, w_avo = self.avoidobstacles.execute(self.ui_params,dt) #execute go-to-goal
+
+        v = (v_gtg + v_avo)/2
+        w = w_gtg*(1-weight_avo) + w_avo*weight_avo
+
+        vl, vr = self.uni2diff((v,w))
+        return (vl, vr) 
