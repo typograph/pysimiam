57d7533cf3f2e2d60803843d66bdea2dd9861657
Tim Fuchs <typograph@elec.ru>
2013-03-02 00:20:24 +0100

    One more try at improving dynamic module reloading

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -5,18 +5,23 @@ class Struct:
         return "{{{}}}".format(", ".join(["{} : {}".format(k,v) for k,v in self.__dict__.items()]))
         #return str(self.__dict__)
 
-__loaded_modules = []
-        
+__loaded_modules = set()
+
 def unload_user_modules():
+    global __loaded_modules
     while __loaded_modules:
         module = __loaded_modules.pop()
         if module in sys.modules:
+            del sys.modules[module]
+    for module in list(sys.modules):
+        if sys.modules[module] is None:
             del sys.modules[module]
         
 def load_by_name(module_string, path = None):
     """Loads a module to the code by name string.
     @params: module_string - module.ModuleName, 
     path - path to module"""
+    global __loaded_modules
     try:
         filename, class_name = module_string.split(".")
     except ValueError:
@@ -29,12 +34,10 @@ def load_by_name(module_string, path = None):
         if path is not None:
             old_modules = set(sys.modules)
             module = __import__(path, globals(), locals(), [filename]).__dict__[filename]
-            for module_name in set(sys.modules) - old_modules:
-                __loaded_modules.append(module_name)
-            #__loaded_modules.append("{}.{}".format(path,filename))
+            __loaded_modules = __loaded_modules.union(set(sys.modules) - old_modules)
         else:
             module = __import__(filename)
-            __loaded_modules.append(filename)
+            __loaded_modules.add(filename)
         controller_class = module.__dict__[class_name]
         return (module, controller_class)
     except ImportError:
