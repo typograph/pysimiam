5e4ee3a8be0d1087ebdbc561121e586532858152
Tim Fuchs <typograph@elec.ru>
2014-02-02 08:40:32 +0100

    Made plotting fail without raising an exception in case the library doesn't load properly.

--- gui/qt_plotwindow.py
+++ gui/qt_plotwindow.py
@@ -1,11 +1,47 @@
 from PyQt4 import QtGui
 from PyQt4.QtCore import pyqtSlot, pyqtSignal, Qt
 
-import pyqtgraph as pg
 from random import randint
+try:
+    import pyqtgraph as pg
+    pg.setConfigOption('background', 'w')
+    pg.setConfigOption('foreground', 'k')
+    USE_PLOTS = True
+    
+    class PlotWindow(pg.GraphicsWindow):
+        """
+        The window consists of a figure with a nav toolbar and subplots.
+        It keeps track of all subplots
+        """
+        
+        def __init__(self):
+            pg.GraphicsWindow.__init__(self)
 
-pg.setConfigOption('background', 'w')
-pg.setConfigOption('foreground', 'k')
+            self.plots = []
+        
+        #Slots
+        def clear_data(self):
+            for plot in self.plots:
+                plot.clear_data()
+            
+        def add_plot(self):
+            """Add a new subplot with a curve given by expression"""
+            n = len(self.plots)
+            if n > 0:
+                self.nextRow()
+            self.plots.append(Plot(self.addPlot()))
+            if n > 0:
+                self.plots[-1].plot.setXLink(self.plots[0].plot)
+                
+            return self.plots[-1]
+        
+        def add_data(self,data):
+            for plot in self.plots:
+                plot.add_data(data)
+    
+except:
+    USE_PLOTS = False
+    print("The plotting library could not be loaded.")
 
 def get_color(color):
     if color is None:
@@ -80,39 +116,10 @@ class Plot:
         for v in self.variables:
             v.clear_data()
 
-class PlotWindow(pg.GraphicsWindow):
-    """
-    The window consists of a figure with a nav toolbar and subplots.
-    It keeps track of all subplots
-    """
-    
-    def __init__(self):
-        pg.GraphicsWindow.__init__(self)
-
-        self.plots = []
-    
-    #Slots
-    def clear_data(self):
-        for plot in self.plots:
-            plot.clear_data()
-        
-    def add_plot(self):
-        """Add a new subplot with a curve given by expression"""
-        n = len(self.plots)
-        if n > 0:
-            self.nextRow()
-        self.plots.append(Plot(self.addPlot()))
-        if n > 0:
-            self.plots[-1].plot.setXLink(self.plots[0].plot)
-            
-        return self.plots[-1]
-    
-    def add_data(self,data):
-        for plot in self.plots:
-            plot.add_data(data)
-
 def create_predefined_plot_window(plots):
     """Create a window with plots from plot dictionary"""
+    if not USE_PLOTS:
+        return None, None
     w = PlotWindow()
     es = []
     for plot in plots:
@@ -121,99 +128,3 @@ def create_predefined_plot_window(plots):
             p.add_curve(l,e,c)        
             es.append(e)
     return es, w
-
-#def create_plot_window(plotables):
-    #"""Create a dialog for plot creation.
-       #Return selected expressions
-    #"""
-    #dialog = PlotDialog(plotables)
-    #if dialog.exec_():
-        ## Create plots
-        #return dialog.expressions(), dialog.plot()
-    #return None, None
-
-#class PlotableComboBox(QtGui.QComboBox):
-    #def __init__(self,plotables,parent):
-        #QtGui.QComboBox.__init__(self,parent)
-        #self.plotables = plotables
-        #for label in plotables:
-            #self.addItem(label)
-        #self.setEditable(True)
-        #self.setCurrentIndex(0)
-    
-    #def expression(self):
-        #text = str(self.currentText())
-        #if text not in self.plotables:
-            #return text
-        #return self.plotables[text]
-    
-    #def create_curve(self,plot):
-        #plot.add_curve(str(self.currentText()),self.expression())
-    
-#class SubPlotGroup(QtGui.QGroupBox):
-    #def __init__(self,plotables,parent):
-        #QtGui.QGroupBox.__init__(self,"Subplot",parent)
-        #self.plotables = plotables
-        #self.combos = []
-        
-        #self.vl = QtGui.QVBoxLayout(self)
-        #button = QtGui.QPushButton("Add curve",self)
-        #button.clicked.connect(self.add_curve)
-        #self.vl.addWidget(button)
-        #self.add_curve()
-        
-    #def add_curve(self):
-        #self.combos.append(PlotableComboBox(self.plotables,self))
-        #self.vl.insertWidget(len(self.combos)-1,self.combos[-1])
-    
-    #def expressions(self):
-        #return [c.expression() for c in self.combos]
-    
-    #def create_plot(self,plot):
-        #splot = plot.add_plot()
-        #for combobox in self.combos:
-            #combobox.create_curve(splot)
-
-#class PlotDialog(QtGui.QDialog):
-    #def __init__(self,plotables):
-        #QtGui.QWidget.__init__(self)
-        
-        #self.plotables = plotables
-        
-        #self.vl = QtGui.QVBoxLayout(self)
-        #self.plots = []
-              
-        #hl = QtGui.QHBoxLayout()
-
-        #addplotbutton = QtGui.QPushButton("Add subplot",self)
-        #addplotbutton.clicked.connect(self.add_subplot)
-        #hl.addWidget(addplotbutton)
-        #hl.addItem(
-            #QtGui.QSpacerItem(40, 20,
-                              #QtGui.QSizePolicy.Expanding,
-                              #QtGui.QSizePolicy.Minimum))
-                              
-        #plotbutton = QtGui.QPushButton("Plot",self)
-        #plotbutton.clicked.connect(self.accept)
-        #hl.addWidget(plotbutton)
-
-        #cancelbutton = QtGui.QPushButton("Cancel",self)
-        #cancelbutton.clicked.connect(self.reject)
-        #hl.addWidget(cancelbutton)
-
-        #self.vl.addLayout(hl)
-        
-        #self.add_subplot()
-     
-    #def add_subplot(self):
-        #self.plots.append(SubPlotGroup(self.plotables,self))
-        #self.vl.insertWidget(len(self.plots)-1,self.plots[-1])
-        
-    #def expressions(self):
-        #return [expr for plot in self.plots for expr in plot.expressions()]
-    
-    #def plot(self):
-        #p = PlotWindow()
-        #for plt in self.plots:
-            #plt.create_plot(p)
-        #return p
